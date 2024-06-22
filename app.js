d0b4e5838fd0b6a6c5db06f6718f6a7e1972e8d8
mcrawf9394 <mjc9394@gmail.com>
2024-06-22 10:14:16 -0400

    Changed the allowed domains that can access the api.

--- app.js
+++ app.js
@@ -25,7 +25,14 @@ require('dotenv').config()
 require('./config/passport')
 app.use(passport.initialize())
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    const corsWhitelist = [
+        'https://mcrawf9394.github.io/BlogEdit/',
+        'https://mcrawf9394.github.io/BlogViewing/'
+    ];
+    if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    }
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, Authentication");
