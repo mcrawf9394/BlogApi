f9ee57acf7c78d2733bb234419bfa48319157f28
mcrawf9394 <mjc9394@gmail.com>
2024-06-23 08:43:57 -0400

    Attempt at allowing the new frontend sites to access the api

--- app.js
+++ app.js
@@ -26,12 +26,11 @@ require('./config/passport')
 app.use(passport.initialize())
 app.use((req, res, next) => {
     const corsWhitelist = [
-        'https://resonant-babka-58ff77.netlify.app/',
-        'https://celadon-swan-bacf27.netlify.app/'
+        'https://resonant-babka-58ff77.netlify.app',
+        'https://celadon-swan-bacf27.netlify.app'
     ];
     if (corsWhitelist.indexOf(req.headers.origin) !== -1) {
         res.header('Access-Control-Allow-Origin', req.headers.origin);
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     }
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
